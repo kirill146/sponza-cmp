ab789144e0173a6f2fe6d2f611e753654742b0f5
Kirill Kozlov <drxu59@gmail.com>
2026-03-23 12:05:01 +0500

    Compare masks

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,36 @@ use std::{ffi::OsStr, fs, io, path::Path};
 
 use image_codecs::{self, Image};
 
+fn eq_masks(img_a: &Image, img_b: &Image) -> bool {
+    assert_eq!(img_a.w, img_b.w);
+    assert_eq!(img_a.h, img_b.h);
+    assert_eq!(img_a.depth, 8);
+    assert_eq!(img_b.depth, 8);
+
+    for y in 0..img_a.h as usize {
+        for x in 0..img_a.w as usize {
+            let mask_val = img_a.buf[(y * img_a.w as usize + x) * img_a.channels as usize + 0] as i32;
+
+            for c in 1..img_a.channels as usize {
+                let val = img_a.buf[(y * img_a.w as usize + x) * img_a.channels as usize + c] as i32;
+                if val != mask_val {
+                    return false;
+                }
+            }
+
+            for c in 0..img_b.channels as usize {
+                let val = img_b.buf[(y * img_b.w as usize + x) * img_b.channels as usize + c] as i32;
+                if (val - mask_val).abs() > 2 {
+                    println!("x: {x} y: {y} c: {c} mask_val: {mask_val} val: {val}");
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
 fn compare_textures() -> io::Result<()> {
     let path_mcguire = Path::new("sponza-versions/morgan_mcguire/textures");
     let path_meinl =   Path::new("sponza-versions/frank_meinl/textures");
@@ -42,13 +72,23 @@ fn compare_textures() -> io::Result<()> {
         } else if tga.depth != png.depth {
             println!("Depths don't match: {} vs {} ({} vs {})", tga.depth, png.depth, tga_filename, png_filename);
         } else if is_ddn {
-            // skip
-            // println!("Can't compare normals");
+            // println!("Can't compare normals"); // skip
+        } else if tga.channels != png.channels {
+            if !eq_masks(&tga, &png) {
+                println!("Masks aren't the same ({} channels vs {} channels): ({} vs {})", tga.channels, png.channels, tga_filename, png_filename);
+            } else {
+                println!("Mask comparison: ok");
+            }
+        } else if tga.channels != png.channels {
+            println!("Number of channels don't match: {} vs {} ({} vs {})", tga.channels, png.channels, tga_filename, png_filename);
         } else if tga.buf != png.buf {
-            println!("Pixels don't match ({} vs {})", tga_filename, png_filename);
+            if tga.buf.len() != png.buf.len() {
+                println!("tga_len != png_len: {} != {}", tga.buf.len(), png.buf.len());
+            } else {
+                println!("Pixels don't match ({} vs {})", tga_filename, png_filename);
+            }
         } else {
-            // skip
-            // println!("Images are the same");
+            // println!("Images are the same"); // skip
         }
     }
 
